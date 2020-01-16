e4a01c5b28491f2a77e9c028b5069008e0bed175
krzysztofminko <50458873+krzysztofminko@users.noreply.github.com>
2020-01-16 15:49:20 +0100

    Implement Task
    #14

---
+++ Assets/Scripts/Task.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Task : MonoBehaviour
+{
+	//public List<Subject> subjects;	<-- uncomment when Subject implemented
+	public int value;
+
+	public abstract bool Execute();
+}
