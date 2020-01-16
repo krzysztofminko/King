ae408b402def7fa456c6dba494483801d4bd776c
krzysztofminko <50458873+krzysztofminko@users.noreply.github.com>
2020-01-16 16:33:31 +0100

    Implement King on foot movement
    #33

---
+++ Assets/Scripts/King.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class King : MonoBehaviour
+{
+	[SerializeField]
+	private float walkSpeed = 1;
+	[SerializeField]
+	private float runSpeed = 2;
+
+	private void Update()
+	{
+		float speed = Input.GetButton("Run") ? runSpeed : walkSpeed;
+		Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * speed * Time.deltaTime;
+		if (movement.sqrMagnitude > 0)
+			transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(movement, Vector3.up), 360 * Time.deltaTime);
+		transform.position += movement;
+	}
+}
